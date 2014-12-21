ee930a07a56ab484435dda655bbd3465760c86ac
Frederic Merizen <frederic.merizen@gmail.com>
2014-12-21 13:09:10 +0100

    Push out the special case "no orders"

--- haskell/Lags.hs
+++ haskell/Lags.hs
@@ -14,15 +14,14 @@ landing o = takeOff o + duration o
 type Plan = [[Order]]
 
 profit :: [Order] -> Money
+profit [] = 0
 profit os = maxProfit
     where p = plan os
           maxProfit = case p of
-              [] -> 0
               [os] -> (maximum . map price) os
               [[o], [o']] -> (price o) + (price o')
 
 plan :: [Order] -> Plan
-plan [] = []
 plan [o] = [[o]]
 plan [o, o']
     | (landing o == landing o') = [[o, o']]
