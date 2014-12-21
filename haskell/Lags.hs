6742cefe87ce2e2626eb48299630d2ab6312136a
Frederic Merizen <frederic.merizen@gmail.com>
2014-12-21 15:54:46 +0100

    start induction on landing time

--- haskell/Lags.hs
+++ haskell/Lags.hs
@@ -1,7 +1,9 @@
 module Lags where
 
-import Data.List
+import Control.Arrow
 import Data.Function
+import Data.List
+import Data.Map
 
 type Timestamp = Int
 type Duration = Int
@@ -14,18 +16,25 @@ data Order = Order { takeOff :: Timestamp
 landing :: Order -> Timestamp
 landing o = takeOff o + duration o
 
-data Plan = Plan {orders :: [[Order]] }
+data Plan = Plan { ordersByLanding :: Map Timestamp [Order]
+                 , lastTime :: Timestamp}
 
 profit :: [Order] -> Money
 profit [] = 0
-profit os = maxProfit
+profit os = profitAt (lastTime p)
     where p = plan os
-          maxProfit = case orders p of
-              [os] -> (maximum . map price) os
-              [[o], [o']] -> (price o) + (price o')
+          profitAt :: Timestamp -> Money
+          profitAt t = case Data.Map.lookup t (ordersByLanding p) of
+              Nothing -> 0
+              Just os -> maximum $ Data.List.map profitFor os
+          profitFor :: Order -> Money
+          profitFor o = (price o) + (profitAt $ takeOff o)
 
 plan :: [Order] -> Plan
-plan = Plan . groupBy sameLanding
+plan os = Plan byLanding lastTime
+    where times = sortBy (flip compare) $ nub $ (Data.List.map takeOff os) ++ (Data.List.map landing os)
+          byLanding = fromList $ Data.List.map (landing . head &&& id) $ groupBy sameLanding os
+          lastTime = head times
 
 sameLanding :: Order -> Order -> Bool
 sameLanding = ( == ) `on` landing
