bd2200328b76d105c27349d1267d95768df4b415
Frederic Merizen <frederic.merizen@gmail.com>
2014-12-25 17:09:32 +0100

    Trie based memoization that actually passes SPOJ

--- haskell/Lags/Memo.hs
+++ haskell/Lags/Memo.hs
@@ -17,4 +17,22 @@ profit os = profitAt (head $ times p)
           profitFor = compose (+) price $ profitAt . takeOff
 
 memoize :: (Int -> a) -> (Int -> a)
-memoize f = (map f [0 ..] !!)
+memoize f = (fmap f (naturals 1 0) !!!)
+
+data NaturalTree a = Node a (NaturalTree a) (NaturalTree a)
+
+naturals r n =
+   Node n
+     ((naturals $! r2) $! (n+r))
+     ((naturals $! r2) $! (n+r2))
+        where r2 = 2*r
+
+instance Functor NaturalTree where
+   fmap f (Node a tl tr) = Node (f a) (fmap f tl) (fmap f tr)
+
+Node a tl tr !!! 0 = a
+Node a tl tr !!! n =
+   if odd n
+     then tl !!! top
+     else tr !!! (top-1)
+        where top = n `div` 2
